87b7eb81e9e8c274c935a1757f39c9eb71843784
AshleyWang29 <yelhsa29@seas.upenn.edu>
2024-12-27 21:04:24 -0500

    homepagecss

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
 
-function Home(props) {
-  return <div>Home</div>;
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="content">
+        <h1>Future of Delivery Starts Today</h1>
+        <button className="order-button">Order Now!</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
