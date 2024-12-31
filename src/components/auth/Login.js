b0fcc6461dd746d32e9c380bc18e2cc42ec2aa40
CyberObservers <13816750292@163.com>
2024-12-31 14:40:53 +0800

    fix login navigation and eslint warning

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,57 +1,53 @@
-import React from 'react';
-import { Form, Input, Button, Card, Select } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
-import '../../styles/Login.css';
+import React from "react";
+import { Form, Input, Button, Card, Select } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Login.css";
 
 const { Option } = Select;
 
-const Login = () => {
+const Login = (props) => {
+  const { handleLoggedIn } = props;
   const [form] = Form.useForm();
   const navigate = useNavigate();
 
   const onFinish = (values) => {
-    console.log('Received values:', values);
-    if (values.userType === 'admin') {
-      navigate('/admin/dashboard');
+    console.log("Received values:", values);
+    handleLoggedIn("test-token");
+    if (values.userType === "admin") {
+      navigate("/dashboard");
     } else {
-      navigate('/user/dashboard');
+      navigate("/home");
     }
   };
 
   const handleSignUpClick = () => {
-    navigate('/Register');
+    navigate("/Register");
   };
 
   return (
     <div className="login-container">
       <div className="login-box">
         <h1>Welcome Back</h1>
-        <Card
-          className="login-card"
-          bordered={false}
-        >
+        <Card className="login-card" bordered={false}>
           <div className="login-header">
             <span className="active">Login</span>
             <span className="divider">|</span>
-            <span 
-              className="inactive" 
+            <span
+              className="inactive"
               onClick={handleSignUpClick}
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: "pointer" }}
             >
               Sign Up
             </span>
           </div>
-          
-          <Form
-            form={form}
-            name="login"
-            onFinish={onFinish}
-            layout="vertical"
-          >
+
+          <Form form={form} name="login" onFinish={onFinish} layout="vertical">
             <Form.Item
               name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
             >
               <Input
                 prefix={<UserOutlined />}
@@ -62,7 +58,9 @@ const Login = () => {
 
             <Form.Item
               name="password"
-              rules={[{ required: true, message: 'Please input your password!' }]}
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
             >
               <Input.Password
                 prefix={<LockOutlined />}
@@ -73,26 +71,26 @@ const Login = () => {
 
             <Form.Item
               name="userType"
-              rules={[{ required: true, message: 'Please select user type!' }]}
+              rules={[{ required: true, message: "Please select user type!" }]}
             >
-              <Select
-                placeholder="Select user type"
-                size="large"
-              >
+              <Select placeholder="Select user type" size="large">
                 <Option value="user">User</Option>
                 <Option value="admin">Admin</Option>
               </Select>
             </Form.Item>
 
             <div className="login-links">
-              <a className="forgot-link">forgot password</a>
-              <a 
-                className="signup-link" 
+              <Button type="link" className="forgot-link">
+                forgot password
+              </Button>
+              <Button
+                type="link"
+                className="signup-link"
                 onClick={handleSignUpClick}
-                style={{ cursor: 'pointer' }}
+                style={{ cursor: "pointer" }}
               >
                 Sign Up
-              </a>
+              </Button>
             </div>
 
             <Form.Item>
