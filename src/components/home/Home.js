929dfe76d72e30c2bc1f43862d09dc5a5c9e56aa
CyberObservers <13816750292@163.com>
2025-01-01 16:04:14 +0800

    change to mui typography

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Typography from "@mui/material/Typography";
 import "../../styles/Home.css";
+
+const typoStyle = {
+  color: "white",
+  fontSize: { xs: "1cm", md: "1cm", lg: "2cm" },
+  fontWeight: "bold",
+  fontFamily: "Roboto monospace",
+};
 
 function Home() {
   const navigate = useNavigate();
@@ -11,7 +19,7 @@ function Home() {
   return (
     <div className="home-container">
       <div className="content">
-        <h1>Future of Delivery Starts Today</h1>
+        <Typography sx={typoStyle}>Future of Delivery Starts Today</Typography>
         <button className="order-button" onClick={handleClick}>
           Order Now!
         </button>
